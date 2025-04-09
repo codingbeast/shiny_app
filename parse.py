dbdd1a5fd3e97d704de22f736ba57c313d37b22a
Raaj <raj0kumar00@gmail.com>
2025-04-09 22:32:02 +0530

    feature : upload output data to google drive

--- parse.py
+++ parse.py
@@ -3,9 +3,10 @@ import pandas as pd
 import os
 from date import OSACDateCSVProcessor
 from country import OSACCountryProcessor
+from scraper import DriveManager,EXTRACTED_DETAILS_CSV_FILE_NAME
 
 
-class DataParser:
+class DataParser(DriveManager):
     def __init__(self, csv_path : str, csv_output_path : str):
         super().__init__()
         if not os.path.exists(csv_path):
@@ -21,6 +22,9 @@ class DataParser:
     def save_df(self, df : pd.DataFrame) -> None:
         #df = df[['OSAC_Title', "country"]]
         df.to_csv(self.csv_output_path,index=False, encoding='utf-8')
+    def upload_to_drive(self, df : pd.DataFrame):
+        csv_data = df.to_dict(orient="records")
+        self.write_csv_to_drive(file_name=EXTRACTED_DETAILS_CSV_FILE_NAME, data_list=csv_data, append=False)
 
 if __name__ == "__main__":
     data_parser = DataParser("osac.csv","osac_test.csv")
@@ -28,6 +32,7 @@ if __name__ == "__main__":
     df_with_date = OSACDateCSVProcessor(df).extract
     df_with_country = OSACCountryProcessor(df_with_date).extract
     data_parser.save_df(df_with_country)
+    data_parser.upload_to_drive(df_with_country)
 
     
 
