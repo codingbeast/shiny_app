05e68c350de427fd52b372cc4fc78b95ca6d88f1
Raaj <raj0kumar00@gmail.com>
2025-04-29 21:31:43 +0530

    removed debug code

--- parse.py
+++ parse.py
@@ -44,7 +44,6 @@ if __name__ == "__main__":
     df_with_aniticipation = OSACDateAnticipationProcessor(df_with_suppression).extract
     data_parser.save_df(df_with_aniticipation)
     data_parser.upload_to_drive(df_with_aniticipation, filename="OSAC_parsed.csv")
-    
     aggregate_parser = OSACAggregateProcessor("OSAC_parsed.csv", "ISO_country_names.txt") #input the parseed csv file
     df_with_daily_data = aggregate_parser.extract_daily_data
     df_with_monthly_data = aggregate_parser.extract_monthly_data
